8378be00c0086f5a3475c8ab0c008246c82de498
CameronWiggins <cw1101046@student.francistuttle.edu>
2023-10-09 10:57:08 -0500

    cloneable and its methods work

--- src/App.java
+++ src/App.java
@@ -3,7 +3,6 @@ public class App {
         Vector2 v1 = new Vector2(2, 2,"red");
         Vector2 v2 = new Vector2(2, 2,"red");
         Vector2 v3 = new Vector2(5, 6,"blue");
-
         System.out.println(v1.magnitude());
         System.out.println(v2);
         System.out.println(v3);
@@ -11,5 +10,8 @@ public class App {
         System.out.println(v2.isEquals(v3));
         Vector2 v4 = v1.combine(v2);
         System.out.println(v4);
+        Vector2 v5 = v4.clone();
+        System.out.println(v5);
+        
     }
 }
