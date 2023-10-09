706a3c4a589e608e331ebfb62cbaa74af89b7223
CameronWiggins <cw1101046@student.francistuttle.edu>
2023-10-09 10:02:46 -0500

    toString and isEquals are complete

--- src/App.java
+++ src/App.java
@@ -1,7 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Vector2 v1 = new Vector2(2, 2);
+        Vector2 v2 = new Vector2(2, 2);
+        Vector2 v3 = new Vector2(5, 6);
 
         System.out.println(v1.magnitude());
+        System.out.println(v2);
+        System.out.println(v3);
+        System.out.println(v2.isEquals(v1));
+        System.out.println(v2.isEquals(v3));
     }
 }
