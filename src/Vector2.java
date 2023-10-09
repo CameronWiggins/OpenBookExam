62ce8e7cb7417a3069c81312fcbb4b1ee7ca3a57
CameronWiggins <cw1101046@student.francistuttle.edu>
2023-10-09 10:00:41 -0500

    testing isEquals and toString

--- src/Vector2.java
+++ src/Vector2.java
@@ -9,8 +9,35 @@ public class Vector2
         yAxis = y;
     }
 
+    public int getX()
+    {
+        return xAxis;
+    }
+
+    public int getY()
+    {
+        return yAxis;
+    }
+
     public double magnitude()
     {
         return Math.sqrt((xAxis * xAxis) + (yAxis * yAxis));
+    }
+
+    public boolean isEquals(Vector2 other)
+    {
+        if((this.getX() == other.getX()) && (this.getY() == other.getY()))
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+
+    public String toString()
+    {
+        return "X Axis is " + xAxis + " and Y Axis is " + yAxis;
     }
 }
